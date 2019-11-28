4b563ebd562b59b8f57b9d9c5335b0bf34022e20
sea <48181984684@qq.com>
2019-11-28 19:21:32 +0800

    test

--- src/views/circle/index.vue
+++ src/views/circle/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     圈子首页
+    {{msg}}
   </div>
 </template>
 <script>
 export default {
-  name: 'circle-home'
+  name: 'circle-home',
+  data() {
+    return {
+      msg: '123'
+    }
+  },
 }
 </script>
