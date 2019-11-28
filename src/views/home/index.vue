9471ccd2723a2e060789a79ea9a46a7244c007f4
EchoesR <937774837@qq.com>
2019-11-28 16:25:09 +0800

    增加拖拽进度条组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,19 @@
   <div>
     home
     {{msg}}
+    <drag-process :min="0" :max="100" v-model="proces" />
   </div>
 </template>
 <script>
+import DragProcess from '_c/DragProcess'
 export default {
   name: 'home',
+  components: {
+    DragProcess
+  },
   data() {
     return {
+      proces: 10,
       msg: '132'
     }
   }
