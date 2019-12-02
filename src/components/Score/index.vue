9f2bb7a109da0a1a0962a7d9bed7bd932e6dc7b4
yanxu <2250317861@qq.com>
2019-12-02 19:50:10 +0800

    积分商城修改后 12/2

---
+++ src/components/Score/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="score_home">
+      <ul>
+        <template v-for="(item, index) in 10">
+          <li :key="index" :class="[item <= parseInt(score) ? trueStyle:falseStyle]">{{item}}</li>
+        </template>
+      </ul>
+      <div>{{score}}分</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "score",
+  props: {
+    score: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      trueStyle: "trueStyle",
+      falseStyle: "falseStyle"
+    };
+  },
+  methods: {}
+};
+</script>
+<style lang='less' scoped>
+ul {
+  width: 350px;
+  height: 31px;
+  display: flex;
+  justify-content: flex-start;
+  li:nth-child(n) {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(229, 229, 229, 1);
+    text-align: center;
+    line-height: 31px;
+    border-right: 1px solid white;
+    color: black;
+    font-size: 14px;
+  }
+}
+.trueStyle {
+  background-color: rgba(3, 92, 193, 1) !important;
+  color: white !important;
+}
+.score_home {
+  display: flex;
+  align-items: center;
+  div:nth-child(2) {
+    margin-left: 10px;
+    color: rgba(212, 48, 48, 1);
+    font-size: 14px;
+    line-height: 150%;
+  }
+}
+</style>
--- src/views/mall/mallHomeRight/xiaoShou.vue
+++ src/views/mall/mallHomeRight/xiaoShou.vue
@@ -4,20 +4,11 @@
     <div class="right_top">
       <!-- title -->
       <div class="right_top_title">
-        <div>销售冠军</div>
-        <div>人气冠军</div>
+        <div @click="xiaoshou">销售冠军</div>
+        <div @click="renqi">人气冠军</div>
       </div>
-      <div class="right_top_img">
-        <img src="@/assets/mall/1336.jpg" />
-      </div>
-      <div class="right_top_text">Xiaomi/小米 小米手机4c高配版/移</div>
-      <div class="right_top_down">
-        <div class="right_top_down_text">
-          <div>8999金币</div>
-          <div>已兑823件</div>
-        </div>
-        <input type="button" value="立即兑换" />
-      </div>
+      <!-- 三级路由 -->
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -27,48 +18,22 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {
+    xiaoshou() {
+      this.$router.push("/mall");
+    },
+    renqi() {
+      console.log(this.$route)
+      console.log(this.$router)
+      this.$router.push("/mall/threeRouter");
+    }
+  }
 };
 </script>
 <style lang='less' scoped>
-.right_top_img {
-  padding: 10px;
-  box-sizing: border-box;
-  img {
-    width: 100%;
-  }
-}
-.right_top_text {
-  font-weight: bold;
-  font-size: 10px;
-  text-align: center;
-  color: rgba(0, 145, 255, 1);
-}
-.right_top_down {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  border-top: 1px solid rgba(0, 145, 255, 0.3);
-  padding: 5px;
-  div {
-    font-size: 10px;
-    font-weight: bold;
-    color: rgba(83, 200, 75, 1);
-  }
-  div:nth-child(n) {
-    margin-left: 10px;
-  }
-  input {
-    border: none;
-    border-radius: 3px;
-    color: rgba(255, 255, 255, 1);
-    background-color: rgba(255, 111, 35, 1);
-    padding: 5px 3px;
-  }
-}
 .right_top {
-  border: 1px solid rgba(0, 145, 255, 0.3);
+  border-radius: 6px;
+  box-shadow:rgba(198, 198, 198, 0.5) 0px 0px 5px;
   .right_top_title {
     display: flex;
     align-items: center;
@@ -85,5 +50,8 @@ export default {
       background-color: rgba(229, 229, 229, 1);
     }
   }
+}
+.myActive{
+  background-color: none;
 }
 </style>
