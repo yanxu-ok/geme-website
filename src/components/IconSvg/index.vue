236173de51cefd5c8b9e5ffcfdcb0da78e8ed69c
chenyang955 <haoyumohe@163.com>
2019-12-04 19:26:48 +0800

    发帖  圈子详情（部分）

--- src/components/IconSvg/index.vue
+++ src/components/IconSvg/index.vue
@@ -26,3 +26,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+.icon-svg {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
